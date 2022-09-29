323a750478039eb556530a2740f3c888fa8ee494
lusine <luskivi218@gmail.com>
2022-09-29 17:08:21 +0400

    CMakeLists.txt written for infix_to_postfix

---
+++ infix_to_postfix/build/CMakeFiles/exec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exec.dir/infix_to_postfix.cpp.o"
+  "exec"
+  "exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
