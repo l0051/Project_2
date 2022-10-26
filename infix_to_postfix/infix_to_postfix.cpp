9dd425708439428b6cc4ae0705c84f23c54702ae
lusine <luskivi218@gmail.com>
2022-10-26 12:35:54 +0400

    infix_to_postfix modified

--- infix_to_postfix/infix_to_postfix.cpp
+++ infix_to_postfix/infix_to_postfix.cpp
@@ -4,71 +4,70 @@
 
 bool is_operator(char symbol)
 {
-	return symbol == '*' || symbol == '+' || symbol == '-'
-		|| symbol == '/';
+    return symbol == '*' || symbol == '+' || symbol == '-' || symbol == '/';
 }
 
 int priority(char current_operator)
 {
-	return (current_operator == '*' || current_operator == '/');
+    return (current_operator == '*' || current_operator == '/');
 }
 
 std::string infix_to_postfix(const std::string& infix)
 {
-	std::stack<char> st;
-	std::string postfix;
+    std::stack<char> st;
+    std::string postfix;
     bool is_last_digit = false;
-	for (char current : infix)
-	{
-		if (is_operator(current))
-		{
+    for (char current : infix)
+    {
+        if (is_operator(current))
+        {
             is_last_digit = false;
-			while (!st.empty() && st.top() != '(' && priority(st.top()) >= priority(current))
-			{
+            while (!st.empty() && st.top() != '(' && priority(st.top()) >= priority(current))
+            {
                 postfix.push_back(' ');
-				postfix.push_back(st.top());
-				st.pop();
-			}
-			st.push(current);
-		}
-		else if (current == '(')
-		{
+                postfix.push_back(st.top());
+                st.pop();
+            }
+            st.push(current);
+        }
+        else if (current == '(')
+        {
             is_last_digit = false;
-			st.push(current);
-		}
-		else if (current == ')')
-		{
+            st.push(current);
+        }
+        else if (current == ')')
+        {
             is_last_digit = false;
-			while (!st.empty() && st.top() != '(')
-			{
+            while (!st.empty() && st.top() != '(')
+            {
                 if (!postfix.empty())
                     postfix.push_back(' ');
-				postfix.push_back(st.top());
+                postfix.push_back(st.top());
                 st.pop();
-			}
-			st.pop();
-		}
-		else if (current != ' ')
-		{
+            }
+            st.pop();
+        }
+        else if (current != ' ')
+        {
             if (!is_last_digit && !postfix.empty())
                 postfix.push_back(' ');
             postfix.push_back(current);
             is_last_digit = true;
-		}
-	}
-	while (!st.empty())
-	{
+        }
+    }
+    while (!st.empty())
+    {
         postfix.push_back(' ');
-		postfix.push_back(st.top());
-		st.pop();
-	}
-	return postfix;
+        postfix.push_back(st.top());
+        st.pop();
+    }
+    return postfix;
 }
 
 int main()
 {
-	std::string infix, postfix;
-	std::getline(std::cin, infix);
-	postfix = infix_to_postfix(infix);
-	std::cout << postfix;
+    std::string infix, postfix;
+    std::getline(std::cin, infix);
+    postfix = infix_to_postfix(infix);
+    std::cout << postfix;
 }
--- postfix_to_infix/postfix_to_infix.cpp
+++ postfix_to_infix/postfix_to_infix.cpp
@@ -4,48 +4,46 @@
 
 bool is_operator(char symbol)
 {
-        return symbol == '*' || symbol == '+' || symbol == '-'
-                || symbol == '/';
+    return symbol == '*' || symbol == '+' || symbol == '-' || symbol == '/';
 }
 
 int priority(char current_operator)
 {
-        return (current_operator == '*' || current_operator == '/');
+    return (current_operator == '*' || current_operator == '/');
 }
 
 std::string postfix_to_infix(const std::string& postfix)
 {
-	std::stack<std::string> st;
-	char last_operation_priority = 0;
-	for (char current : postfix)
-	{
-		if (is_operator(current))
-		{	
-			std::string current_operation = st.top();
-			st.pop();
-			st.top().push_back(current);
-			if (last_operation_priority < priority(current))
-				current_operation = "(" + current_operation + ")";
-			current_operation = st.top() + current_operation;
-			st.pop();
-			st.push(current_operation);
-			last_operation_priority = priority(current);
-		}
-		else
-		{
-			st.push(std::string(1, current));
-		}
-	}
-	if (st.empty())
-		return "";
-	return st.top();
+    std::stack<std::string> st;
+    char last_operation_priority = 1;
+    for (char current : postfix)
+    {
+        if (is_operator(current)) {
+            std::string current_operation = st.top();
+            st.pop();
+//            if (last_operation_priority < priority(current))
+//                current_operation = "(" + st.top() + ")" + current + current_operation;
+//            else
+            current_operation = st.top() + current + current_operation;
+            st.pop();
+            st.push(current_operation);
+            last_operation_priority = priority(current);
+        }
+        else
+        {
+            st.push(std::string(1, current));
+        }
+    }
+    if (st.empty())
+        return "";
+    return st.top();
 }
 
 int main()
 {
-        std::string infix, postfix;
-        std::getline(std::cin, postfix);
-        infix = postfix_to_infix(postfix);
-        std::cout << infix;
+    std::string infix, postfix;
+    std::getline(std::cin, postfix);
+    infix = postfix_to_infix(postfix);
+    std::cout << infix;
 }
 
